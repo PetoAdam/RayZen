8732de36553acf1b4c0cf535e47333988c79e945
PetoAdam <peto.adam@yahoo.com>
2025-01-16 03:51:48 +0100

    new approach init

--- RayZen/src/main.cpp
+++ RayZen/src/main.cpp
@@ -1,163 +1,64 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <float.h>
-#include <cmath>
 
-#include "Ray.h"
-#include "Sphere.h"
-#include "Scene.h"
-#include "Camera.h"
-#include "Material.h"
-#include "Light.h"
-
-// Constants
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-const int MAX_SPHERES = 10;
-const int MAX_MATERIALS = 10;
-
-// Function prototypes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-GLuint loadShaders(const char* vertexPath, const char* fragmentPath);
-void setupFramebuffers(GLuint &framebufferA, GLuint &framebufferB, GLuint &textureA, GLuint &textureB, GLuint &rbo);
-void renderToFramebuffer(GLuint framebuffer, GLuint shaderProgram, GLuint quadVAO);
-void displayFramebuffer(GLuint texture, GLuint shaderProgram, GLuint quadVAO);
-
-// Global variables
-GLuint framebufferA, framebufferB;
-GLuint textureA, textureB;
-GLuint rbo;
-GLuint quadVAO, quadVBO;
-GLuint shaderProgram;
-
-void sendSceneDataToShader(GLuint shaderProgram, const Camera& camera, const std::vector<Sphere>& spheres, const std::vector<Material>& materials) {
-    glUseProgram(shaderProgram);
-
-    GLint viewMatrixLoc = glGetUniformLocation(shaderProgram, "camera.viewMatrix");
-    if (viewMatrixLoc == -1) std::cerr << "Uniform 'camera.viewMatrix' not found!" << std::endl;
-
-    GLint projMatrixLoc = glGetUniformLocation(shaderProgram, "camera.projectionMatrix");
-    if (projMatrixLoc == -1) std::cerr << "Uniform 'camera.projectionMatrix' not found!" << std::endl;
-
-    GLint camPosLoc = glGetUniformLocation(shaderProgram, "camera.position");
-    if (camPosLoc == -1) std::cerr << "Uniform 'camera.position' not found!" << std::endl;
-
-    glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(camera.viewMatrix));
-    glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, glm::value_ptr(camera.projectionMatrix));
-    glUniform3fv(camPosLoc, 1, glm::value_ptr(camera.position));
-
-    GLint spheresLoc = glGetUniformLocation(shaderProgram, "spheres");
-    if (spheresLoc == -1) std::cerr << "Uniform 'spheres' not found!" << std::endl;
-    glUniform3fv(glGetUniformLocation(shaderProgram, "spheres[0].center"), spheres.size(), glm::value_ptr(spheres[0].center));
-    glUniform1fv(glGetUniformLocation(shaderProgram, "spheres[0].radius"), spheres.size(), &spheres[0].radius);
-    glUniform1iv(glGetUniformLocation(shaderProgram, "spheres[0].materialIndex"), spheres.size(), &spheres[0].materialIndex);
-    if(glGetUniformLocation(shaderProgram, "spheres[0].radius") == -1) std::cerr << "Materialindex not found" << std::endl;
-
-    GLint materialsLoc = glGetUniformLocation(shaderProgram, "materials");
-    glUniform3fv(glGetUniformLocation(shaderProgram, "materials[0].diffuse"), materials.size(), glm::value_ptr(materials[0].diffuse));
-    glUniform3fv(glGetUniformLocation(shaderProgram, "materials[0].specular"), materials.size(), glm::value_ptr(materials[0].specular));
-    glUniform1fv(glGetUniformLocation(shaderProgram, "materials[0].shininess"), materials.size(), &materials[0].shininess);
-    glUniform1fv(glGetUniformLocation(shaderProgram, "materials[0].metallic"), materials.size(), &materials[0].metallic);
-    glUniform1fv(glGetUniformLocation(shaderProgram, "materials[0].fuzz"), materials.size(), &materials[0].fuzz);
-    glUniform3fv(glGetUniformLocation(shaderProgram, "materials[0].albedo"), materials.size(), glm::value_ptr(materials[0].albedo));
-
-    GLenum err = glGetError();
-    if (err != GL_NO_ERROR) {
-        std::cerr << "OpenGL Error: " << err << std::endl;
-    }
+// Shader source (embedded for simplicity)
+const char* vertexShaderSource = R"(
+#version 330 core
+layout(location = 0) in vec2 position;
+out vec2 uv;
+void main() {
+    uv = (position + 1.0) / 2.0;
+    gl_Position = vec4(position, 0.0, 1.0);
 }
+)";
 
-void setupQuad() {
-    GLfloat vertices[] = {
-        // Positions        // Texture Coords
-        -1.0f, -1.0f, 0.0f, 0.0f,
-         1.0f, -1.0f, 1.0f, 0.0f,
-         1.0f,  1.0f, 1.0f, 1.0f,
-        -1.0f,  1.0f, 0.0f, 1.0f
-    };
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
 
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
+struct Sphere {
+    vec3 center;
+    float radius;
+};
 
-    glBindVertexArray(quadVAO);
+uniform Sphere spheres[10];
+uniform int sphereCount;
 
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+in vec2 uv;
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
+void main() {
+    vec3 rayOrigin = vec3(0.0, 0.0, 3.0);
+    vec3 rayDir = normalize(vec3(uv - 0.5, -1.0));
 
-    glBindVertexArray(0);
-}
+    float minDist = 10000.0;
+    vec3 color = vec3(0.0);
 
-void setupFramebuffers(GLuint &framebufferA, GLuint &framebufferB, GLuint &textureA, GLuint &textureB, GLuint &rbo) {
-    glGenFramebuffers(1, &framebufferA);
-    glGenFramebuffers(1, &framebufferB);
-    glGenTextures(1, &textureA);
-    glGenTextures(1, &textureB);
-    glGenRenderbuffers(1, &rbo);
+    for (int i = 0; i < sphereCount; ++i) {
+        vec3 oc = rayOrigin - spheres[i].center;
+        float a = dot(rayDir, rayDir);
+        float b = 2.0 * dot(oc, rayDir);
+        float c = dot(oc, oc) - spheres[i].radius * spheres[i].radius;
+        float discriminant = b * b - 4.0 * a * c;
 
-    // Setup framebufferA
-    glBindFramebuffer(GL_FRAMEBUFFER, framebufferA);
-    glBindTexture(GL_TEXTURE_2D, textureA);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureA, 0);
-
-    // Setup framebufferB
-    glBindFramebuffer(GL_FRAMEBUFFER, framebufferB);
-    glBindTexture(GL_TEXTURE_2D, textureB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureB, 0);
-
-    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        std::cerr << "Framebuffer not complete!" << std::endl;
+        if (discriminant > 0.0) {
+            float t = (-b - sqrt(discriminant)) / (2.0 * a);
+            if (t < minDist && t > 0.0) {
+                minDist = t;
+                color = vec3(1.0, 0.0, 0.0); // Sphere hit (red color)
+            }
+        }
     }
 
-    glBindFramebuffer(GL_FRAMEBUFFER, 0); // Unbind framebuffer
+    FragColor = vec4(color, 1.0);
 }
+)";
 
-void renderToFramebuffer(GLuint framebuffer, GLuint shaderProgram, GLuint quadVAO) {
-    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glUseProgram(shaderProgram);
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    glBindVertexArray(0);
-}
-
-void displayFramebuffer(GLuint texture, GLuint shaderProgram, GLuint quadVAO) {
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glUseProgram(shaderProgram);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    glBindVertexArray(0);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
 
 int main() {
@@ -170,134 +71,105 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Ray Tracing", nullptr, nullptr);
-    if (window == nullptr) {
+    GLFWwindow* window = glfwCreateWindow(800, 600, "CPU-Controlled Spheres", nullptr, nullptr);
+    if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    glewExperimental = true;
     if (glewInit() != GLEW_OK) {
         std::cerr << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
 
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    shaderProgram = loadShaders("../shaders/vertex_shader.glsl", "../shaders/fragment_shader.glsl");
+    // Compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
 
-    setupQuad();
-    setupFramebuffers(framebufferA, framebufferB, textureA, textureB, rbo);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
 
-    Camera camera(
-        glm::vec3(0.0f, 0.0f, 3.0f),
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        70.0f,
-        (float)SCR_WIDTH / (float)SCR_HEIGHT,
-        0.001f,
-        10000.0f
-    );
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
-    std::vector<Material> materials = {
-        Material(glm::vec3(0.8f, 0.3f, 0.3f), glm::vec3(0.9f, 0.9f, 0.9f), 32.0f, 0.1f, 0.0f, glm::vec3(0.8f, 0.3f, 0.3f)),
-        //Material(glm::vec3(0.3f, 0.8f, 0.3f), glm::vec3(0.9f, 0.9f, 0.9f), 64.0f, 0.5f, 0.1f, glm::vec3(0.3f, 0.8f, 0.3f))
+    // Quad vertices for fullscreen rendering
+    float vertices[] = {
+        -1.0f, -1.0f,
+         1.0f, -1.0f,
+         1.0f,  1.0f,
+        -1.0f,  1.0f
+    };
+
+    GLuint indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    // Define spheres
+    struct Sphere {
+        glm::vec3 center;
+        float radius;
     };
 
     std::vector<Sphere> spheres = {
-        Sphere(glm::vec3(0.0f, 0.0f, -1.0f), 0.5f, 0),
-        //Sphere(glm::vec3(1.0f, 0.0f, -1.5f), 0.2f, 1)
+        {{0.0f, 0.0f, -1.0f}, 0.5f},
+        {{0.5f, -0.5f, -1.5f}, 0.3f},
+        {{-0.5f, 0.5f, -2.0f}, 0.4f}
     };
 
-    bool useFramebufferA = true;
+    glUseProgram(shaderProgram);
+    glUniform1i(glGetUniformLocation(shaderProgram, "sphereCount"), spheres.size());
+    for (size_t i = 0; i < spheres.size(); ++i) {
+        std::string base = "spheres[" + std::to_string(i) + "].";
+        glUniform3fv(glGetUniformLocation(shaderProgram, (base + "center").c_str()), 1, glm::value_ptr(spheres[i].center));
+        glUniform1f(glGetUniformLocation(shaderProgram, (base + "radius").c_str()), spheres[i].radius);
+    }
 
     while (!glfwWindowShouldClose(window)) {
-        processInput(window);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        // Render to the framebuffer
-        GLuint currentFramebuffer = useFramebufferA ? framebufferA : framebufferB;
-        GLuint currentTexture = useFramebufferA ? textureA : textureB;
-
-        renderToFramebuffer(currentFramebuffer, shaderProgram, quadVAO);
-
-        // Display the result
-        displayFramebuffer(currentTexture, shaderProgram, quadVAO);
-
-        // Swap framebuffers
-        useFramebufferA = !useFramebufferA;
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteFramebuffers(1, &framebufferA);
-    glDeleteFramebuffers(1, &framebufferB);
-    glDeleteTextures(1, &textureA);
-    glDeleteTextures(1, &textureB);
-    glDeleteRenderbuffers(1, &rbo);
-    glDeleteVertexArrays(1, &quadVAO);
-    glDeleteBuffers(1, &quadVBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-GLuint loadShaders(const char* vertexPath, const char* fragmentPath) {
-    std::ifstream vShaderFile(vertexPath);
-    std::ifstream fShaderFile(fragmentPath);
-    std::stringstream vShaderStream, fShaderStream;
-    vShaderStream << vShaderFile.rdbuf();
-    fShaderStream << fShaderFile.rdbuf();
-    std::string vertexCode = vShaderStream.str();
-    std::string fragmentCode = fShaderStream.str();
-    const char* vShaderCode = vertexCode.c_str();
-    const char* fShaderCode = fragmentCode.c_str();
-
-    GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vShaderCode, NULL);
-    glCompileShader(vertex);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-        std::cerr << "Vertex Shader Compilation Error: " << infoLog << std::endl;
-    }
-
-    GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fShaderCode, NULL);
-    glCompileShader(fragment);
-    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-        std::cerr << "Fragment Shader Compilation Error: " << infoLog << std::endl;
-    }
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertex);
-    glAttachShader(shaderProgram, fragment);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cerr << "Shader Program Linking Error: " << infoLog << std::endl;
-    }
-
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
-
-    return shaderProgram;
 }
