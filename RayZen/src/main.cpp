511e0e5755116bc192674d1afd4ad122ba987b4f
PetoAdam <peto.adam@yahoo.com>
2025-03-03 02:40:08 +0100

    add rotation to the first cube in the scene

--- RayZen/src/main.cpp
+++ RayZen/src/main.cpp
@@ -149,6 +149,9 @@ int main() {
         scene.camera.aspectRatio = float(SCR_WIDTH) / float(SCR_HEIGHT);
         scene.camera.updateProjectionMatrix();
 
+        // Rotate the first cube
+        scene.meshes[0].transform = glm::rotate(scene.meshes[0].transform, 0.01f, glm::vec3(0.0f, 1.0f, 0.0f));
+
         // Send scene data to the shader
         sendSceneDataToShader(shaderProgram, scene);
 
